93d2dabfc978b48daca4f6268a0ea847ef9efc8b
abbasali1994 <abbasali1994>
2024-04-02 16:46:39 +0530

    remove firebase + rudderstack installs

--- .env.d.ts
+++ .env.d.ts
@@ -6,8 +6,7 @@ declare global {
       ZORA_MAINNET_RPC: string;
       BASE_MAINNET_RPC: string;
       ETHERSCAN_API_KEY: string;
-      RUDDERSTACK_WRITE_KEY: string;
-      RUDDERSTACK_DATA_PLANE: string;
+
       SENTRY_DSN: string;
       DATA_API_KEY: string;
       DATA_ENDPOINT: string;
@@ -15,12 +14,6 @@ declare global {
       PLAYGROUND: 'default' | 'ds';
       LOG_LEVEL: 'debug' | 'info' | 'warn' | 'error';
       LOG_DEBUG: string;
-      FIREBASE_API_KEY_BX: string;
-      FIREBASE_AUTH_DOMAIN_BX: string;
-      FIREBASE_PROJECT_ID_BX: string;
-      FIREBASE_SENDER_ID_BX: string;
-      FIREBASE_APP_ID_BX: string;
-      FIREBASE_VAPID_BX: string;
       IMGIX_DOMAIN: string;
       IMGIX_TOKEN: string;
       RPC_PROXY_ENABLED: string;
